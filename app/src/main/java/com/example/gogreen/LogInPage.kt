b1005d4b43c4a38998d24c7891f371946b15ffa3
BentleySoon <2000zan@gmail.com>
2022-03-28 16:26:56 +0800

    working register and login page
    need to convert actual modules to tab view and disable out of app back button.
    
    cannot get username to update to database, need to try because actual app will use database

--- app/src/main/java/com/example/gogreen/LogInPage.kt
+++ app/src/main/java/com/example/gogreen/LogInPage.kt
@@ -1,15 +1,33 @@
 package com.example.gogreen
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
 
 class LogInPage : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private var email: String = ""
+    private var password: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in_page)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        email = findViewById<EditText>(R.id.textLoginEmail).text.toString()
+        password = findViewById<EditText>(R.id.textLoginPass).text.toString()
 
         val btnRegister: Button = findViewById(R.id.toRegisterBtn)
 
@@ -17,5 +35,75 @@ class LogInPage : AppCompatActivity() {
             val intent: Intent = Intent(this, SignUpPage::class.java)
             startActivity(intent)
         }
+
+        val btnLogin: Button = findViewById(R.id.loginBtn)
+
+        btnLogin.setOnClickListener(){
+            email = findViewById<EditText>(R.id.textLoginEmail).text.toString()
+            password = findViewById<EditText>(R.id.textLoginPass).text.toString()
+
+            signIn(email, password)
+        }
+
+
+
+        auth = Firebase.auth
+
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        val i = Intent(this, ActivitiesMainPage::class.java)
+        if (user != null) {
+            i.putExtra("email", user.email)
+        }
+        startActivity(i)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun reload(){
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("email", email)
+        outState.putString("password", password)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        savedInstanceState.putString("email", email)
+        savedInstanceState.putString("password", password)
     }
 }
--- app/src/main/java/com/example/gogreen/SignUpPage.kt
+++ app/src/main/java/com/example/gogreen/SignUpPage.kt
@@ -1,12 +1,163 @@
 package com.example.gogreen
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.example.gogreen.models.User
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+
+import com.google.firebase.ktx.Firebase
 
 class SignUpPage : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
+    private lateinit var auth: FirebaseAuth
+    private var email: String = ""
+    private var username: String = ""
+    private var password: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_page)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        email = findViewById<EditText>(R.id.textRegisterEmail).text.toString()
+        //username = findViewById<EditText>(R.id.textRegisterName).text.toString()
+        password = findViewById<EditText>(R.id.textRegisterPass).text.toString()
+
+        val btnRegister: Button = findViewById(R.id.registerBtn)
+
+
+        btnRegister.setOnClickListener(){
+            email = findViewById<EditText>(R.id.textRegisterEmail).text.toString()
+            //username = findViewById<EditText>(R.id.textRegisterName).text.toString()
+            password = findViewById<EditText>(R.id.textRegisterPass).text.toString()
+
+            if(email.isEmpty()){
+                findViewById<EditText>(R.id.textRegisterEmail).error = "Email is required!"
+                findViewById<EditText>(R.id.textRegisterEmail).requestFocus()
+            }
+            else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                findViewById<EditText>(R.id.textRegisterEmail).error = "Valid email is required!"
+                findViewById<EditText>(R.id.textRegisterEmail).requestFocus()
+            }
+//            else if(username.isEmpty()){
+//                findViewById<EditText>(R.id.textRegisterName).error = "Username is required!"
+//                findViewById<EditText>(R.id.textRegisterName).requestFocus()
+//            }
+            else if(password.isEmpty()){
+                findViewById<EditText>(R.id.textRegisterPass).error = "Password is required!"
+                findViewById<EditText>(R.id.textRegisterPass).requestFocus()
+            }else if (password.length < 6 ) {
+                findViewById<EditText>(R.id.textRegisterPass).error = "Password is too short!"
+                findViewById<EditText>(R.id.textRegisterPass).requestFocus()
+            }
+            else
+            {
+                createAccount(email, password)
+            }
+        }
+    }
+
+
+    public override fun onStart(){
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+
+    }
+
+    private fun createAccount(email:String, password:String){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    finish()
+                    Log.d(TAG, "createUserWithEmail:success")
+                    Toast.makeText(baseContext, "Registration successful.",Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+
+                  // writeNewUser(user?.uid.toString(), username, email)
+
+
+                    //updateUI(user)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Registration failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+
+    }
+
+
+
+    private fun writeNewUser(userId: String, username: String, email: String){
+        val user = User(username, email)
+
+
+        database.child("users").child(userId).setValue(user).addOnCompleteListener(this){
+            task->
+            if(task.isSuccessful){
+                Toast.makeText(baseContext, "Pog.",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(baseContext, "Not Pog.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+
+    private fun reload(){
+
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if(user != null){
+            finish()
+        }else{
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("username", username)
+        outState.putString("email", email)
+        outState.putString("password", password)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        savedInstanceState.getString("username", username)
+        savedInstanceState.putString("email", email)
+        savedInstanceState.putString("password", password)
     }
 }
