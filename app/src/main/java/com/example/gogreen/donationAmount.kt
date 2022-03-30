fa3ed71df0996cd25e59b1bb20532ff0963114ed
BentleySoon <2000zan@gmail.com>
2022-03-30 21:36:29 +0800

    donations tab finished need to add subsequent donation activity
    profile page need work, need add contact me button
    add profile photo
    
    implement donation history

---
+++ app/src/main/java/com/example/gogreen/donationAmount.kt
@@ -0,0 +1,11 @@
+package com.example.gogreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class donationAmount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation_amount)
+    }
+}
